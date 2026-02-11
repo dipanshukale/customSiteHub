fc96806b3af43347041b596af4fad6247f7814a5
dipanshu kale <dipanshukale2003@gmail.com>
2026-02-11 10:29:52 +0530

    some changes did

--- frontend/src/Components/ScrollToTop.tsx
+++ frontend/src/Components/ScrollToTop.tsx
@@ -8,7 +8,7 @@ const ScrollToTop = () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "smooth", // use "smooth" if you want luxury glide
+      behavior: "smooth",
     });
   }, [pathname]);
 
